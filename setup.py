d8ec8d0b0e1a7f9f96d85e90274dc0729a2e2f99
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2018-05-07 16:11:52 +0545

    Added tarball url to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'BsManager',
+  packages = ['BsManager'],
+  version = '0.1',
+  description = 'Native python3 api for bombsquad',
+  author = 'Rajat Parajuli',
+  author_email = 'rjpj@parajuli.me',
+  url = 'https://github.com/rjpj2016/bsManager',
+  download_url = 'https://github.com/rjpj2016/bsManager/archive/0.1.tar.gz',
+  keywords = ['api', 'python3', 'bombsquad'],
+  classifiers = [],
+)
